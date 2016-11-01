fbb11b544ae2dc2fa204b98c16ca1156607a375d
br1pop <brunopoppolo@hotmail.com>
2016-11-01 20:45:28 -0300

    Update routers
    Updeate routers to resolve merge conflict

--- www/js/routers/app_routers.js
+++ www/js/routers/app_routers.js
@@ -2,7 +2,7 @@
 	"use strict";
 	angular.module("myApp").config(function($stateProvider, $urlRouterProvider){
 
-		$urlRouterProvider.otherwise("/home");
+		$urlRouterProvider.otherwise("/notificaciones");
 
 		$stateProvider
 
@@ -12,10 +12,47 @@
             controller:"initCtrl"
 		})
         
-        .state("cambiarPerfil", {
-			url:"/home",
-            templateUrl:"views/cambiarPerfil.html",
-            controller:"initCtrl"
+        .state("mapa", {
+			url:"/mapa",
+            templateUrl:"views/mapa.html",
+            controller:"mapaCtrl"
+		})
+    
+        .state("ppal", {
+			url:"/ppal",
+            templateUrl:"views/ppal.html",
+            controller:"ppalCtrl"
+		})
+        
+                .state("ppal.mapa", {
+                    url:"/mapa",
+                    views:{
+                        'menuContent':{
+                            templateUrl:"views/mapa.html"
+                        }
+                    }
+                })
+        
+                .state("ppal.perfiles", {
+                    url:"/perfiles",
+                    views:{
+                        'menuContent':{
+                            templateUrl:"views/perfiles.html",
+                            controller:"perfilesCtrl"
+                        }
+                    }
+                })
+        
+        .state("perfiles", {
+			url:"/perfiles",
+            templateUrl:"views/perfiles.html",
+            controller:"perfilesCtrl"
+		})
+        
+        .state("notificaciones", {
+			url:"/notificaciones",
+            templateUrl:"views/notificaciones.html",
+            controller:"notificacionesCtrl"
 		})
 
 
