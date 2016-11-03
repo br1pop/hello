7c1eadc7d8f91a2a8ed6777c292ba609a88d5008
br1pop <brunopoppolo@hotmail.com>
2016-11-02 22:22:54 -0300

    Delete www

--- www/js/controller/avisos_controller.js
+++
@@ -1,8 +0,0 @@
-(function(global) {
-	//"use strict";
-    
-   angular.module("myApp").controller("avisosCtrl", function($scope,$rootScope, Data, $ionicModal, $location, DBLocal, DBLocalLoginDeUsuario){
-                   
-    });
-         
-})();
